9cee625567fff8d0111b9e7a2890d06b424ddaab
Harsh <drharshkumar499@gmail.com>
2026-07-24 09:25:04 +0530

    data-types-summary

--- 01_basics/comparisons.js
+++ 01_basics/comparisons.js
@@ -10,3 +10,11 @@ console.log(null===undefined); // false
 console.log(null==0); // false
 console.log(null>0); // false
 console.log(null>=0); // true
+
+/*
+symbols are unique and immutable data types that can be used as identifiers for object properties.
+They are created using the Symbol() function. Each time you create a new symbol, it is guaranteed to be unique.
+*/
+const sym1 = Symbol("description");
+const sym2 = Symbol("description");
+console.log(sym1 === sym2); // false
