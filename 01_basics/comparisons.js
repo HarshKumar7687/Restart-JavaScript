92ed89846b28de11c70697eada4402ced8af14f2
Harsh <drharshkumar499@gmail.com>
2026-07-24 09:08:40 +0530

    comparisons.js

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,12 @@
+console.log(5 > 3); // true
+console.log("2"==2); // true    
+console.log("02"==2); // true
+console.log("02"===2); // false
+console.log("02"!==2); // true
+console.log(null==undefined); // true
+console.log(null===undefined); // false
+
+
+console.log(null==0); // false
+console.log(null>0); // false
+console.log(null>=0); // true
