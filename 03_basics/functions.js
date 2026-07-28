1cda1f5f12e903a4f5c7968c7c0904df38e9051b
Harsh <drharshkumar499@gmail.com>
2026-07-28 23:57:14 +0530

    functions1

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -13,3 +13,30 @@ function addSum(a,b){
 }
 
 console.log(addSum(17,3))
+
+//rest operator looks like spread operator
+function totalCart(...num1){
+    return num1;
+}
+
+console.log(totalCart(100,200,300)); //Output : [ 100, 200, 300 ]
+
+
+let user = {
+    username: "Harsh",
+    age: 21
+}
+
+function handleObject(anyObject){
+    return `The username is ${anyObject.username} and use age is ${anyObject.age}`
+}
+
+console.log(handleObject(user))
+
+
+let arr = [17,3,5,10];
+function getSecondElement(array){
+    return array[1]
+}
+
+console.log(getSecondElement(arr))
