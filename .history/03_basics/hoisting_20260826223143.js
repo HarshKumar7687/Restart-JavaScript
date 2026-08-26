b953049b6bac03854c528445a30b2c141e9ad858
Harsh <drharshkumar499@gmail.com>
2026-08-26 22:37:18 +0530

    hoisting added

--- .history/03_basics/hoisting_20260826223143.js
+++
@@ -1,4 +0,0 @@
-/*
-Hoisting is JavaScript's behavior of processing declarations before executing the code, allowing function declarations and var
-variables to be referenced before their declaration, with important differences for let, const, and function expressions.
-*/
--- .history/03_basics/hoisting_20260826223250.js
+++
@@ -1,8 +0,0 @@
-/*
-Hoisting is JavaScript's behavior of processing declarations before executing the code, allowing function declarations and var
-variables to be referenced before their declaration, with important differences for let, const, and function expressions.
-*/
-
-function addOne(num){
-    return num + 1;
-}
