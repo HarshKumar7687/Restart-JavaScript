a8a20d4078499e481f91f39b7f32c2bf4ad13735
Harsh <drharshkumar499@gmail.com>
2026-08-30 20:37:09 +0530

    while loops

---
+++ 05_iterations/whileLoops.js
@@ -0,0 +1,14 @@
+//7:40:00
+
+let i = 1;
+while(i < 11) {
+    console.log(`2 * ${i} : ${i*2}`);
+    i++;
+}
+
+let array = ["Spiderman", "Ironman", "Hulk", "Thor", "Captain America"];
+let j = 0;
+while(j < array.length) {
+    console.log(`Hero ${j} : ${array[j]}`);
+    j++;
+}
