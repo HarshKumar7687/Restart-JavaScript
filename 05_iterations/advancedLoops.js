25e52947fe65f7e07bf9c47895e7f6368916840c
Harsh <drharshkumar499@gmail.com>
2026-08-30 20:50:09 +0530

    for of

---
+++ 05_iterations/advancedLoops.js
@@ -0,0 +1,10 @@
+// for of loop
+const array = ["Spiderman", "Ironman", "Hulk", "Thor", "Captain America"];
+for(const hero of array) {
+    console.log(`Hero : ${hero}`);
+}
+
+const greet = "Hello World";
+for(const char of greet) {
+    console.log(`Character : ${char}`);
+}
