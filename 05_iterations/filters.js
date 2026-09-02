f41e38d8e5486f02997f12ab4388b4aeed3b3e28
Harsh <drharshkumar499@gmail.com>
2026-09-02 22:25:00 +0530

    filter.js

--- 05_iterations/filters.js
+++ 05_iterations/filters.js
@@ -20,3 +20,105 @@ numbers.forEach((number) => {
     }
 });
 console.log(evenNumbersForEach); // [2, 4, 6, 8, 10]
+
+
+const superheroes = [
+  {
+    id: 1,
+    name: "Iron Man",
+    realName: "Tony Stark",
+    power: "Technology",
+    team: "Avengers",
+    age: 48,
+    isAlive: false
+  },
+  {
+    id: 2,
+    name: "Captain America",
+    realName: "Steve Rogers",
+    power: "Super Strength",
+    team: "Avengers",
+    age: 106,
+    isAlive: true
+  },
+  {
+    id: 3,
+    name: "Thor",
+    realName: "Thor Odinson",
+    power: "Lightning",
+    team: "Avengers",
+    age: 1500,
+    isAlive: true
+  },
+  {
+    id: 4,
+    name: "Spider-Man",
+    realName: "Peter Parker",
+    power: "Spider Abilities",
+    team: "Avengers",
+    age: 28,
+    isAlive: true
+  },
+  {
+    id: 5,
+    name: "Black Widow",
+    realName: "Natasha Romanoff",
+    power: "Espionage",
+    team: "Avengers",
+    age: 39,
+    isAlive: false
+  },
+  {
+    id: 6,
+    name: "Superman",
+    realName: "Clark Kent",
+    power: "Super Strength",
+    team: "Justice League",
+    age: 35,
+    isAlive: true
+  },
+  {
+    id: 7,
+    name: "Batman",
+    realName: "Bruce Wayne",
+    power: "Intelligence",
+    team: "Justice League",
+    age: 40,
+    isAlive: true
+  },
+  {
+    id: 8,
+    name: "Wonder Woman",
+    realName: "Diana Prince",
+    power: "Super Strength",
+    team: "Justice League",
+    age: 800,
+    isAlive: true
+  },
+  {
+    id: 9,
+    name: "The Flash",
+    realName: "Barry Allen",
+    power: "Super Speed",
+    team: "Justice League",
+    age: 30,
+    isAlive: true
+  },
+  {
+    id: 10,
+    name: "Aquaman",
+    realName: "Arthur Curry",
+    power: "Underwater Strength",
+    team: "Justice League",
+    age: 35,
+    isAlive: true
+  }
+];
+
+const aliveSuperheroes = superheroes.filter((hero) => hero.isAlive);
+console.log(aliveSuperheroes); // returns an array of superheroes that are alive
+
+const marvelSuperheroes = superheroes.filter((hero)=>{
+    return hero.team === "Avengers";
+});
+console.log(marvelSuperheroes); // returns an array of superheroes that are in the Avengers team
